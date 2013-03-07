3d7dba1df10f518137ed14cac9d46573b1d83eb3
rfenarol <fenaroliroberto@live.it>
2013-03-07 14:18:02 +0100

    aggiunte tablature

--- 2013-03-05/exercise01.js
+++ 2013-03-05/exercise01.js
@@ -1,7 +1,7 @@
 var n = '';
 for (var r=1; r<11; r++) {
 	for(var c=1; c<11; c++){
-		n += r*c + ' ';
+		n += r*c + '\t';
 	}
 	console.log(n);
 	n='';
--- 2013-03-05/exercise02.js
+++ 2013-03-05/exercise02.js
@@ -2,7 +2,7 @@ var n = '';
 for (var r=1; r<11; r++) {
 	for(var c=1; c<11; c++){
 		if(c<10){
-			n += r*c + ',' + ' ';	
+			n += r*c + ',\t';	
 		}else{
 			n += r*c;	
 		}
