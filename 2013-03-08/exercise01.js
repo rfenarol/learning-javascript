a85eda20986d6adfe6a9f17b3f33ce81667179c1
rfenarol <fenaroliroberto@live.it>
2013-03-08 13:02:28 +0100

    corretti esercizi 08 marzo

--- 2013-03-08/exercise01.js
+++ 2013-03-08/exercise01.js
@@ -3,9 +3,8 @@ function pushToArray(n){
 	for(var c=1; c<=n; c++){
 		array.push(c);
 	}
-	var array = array.filter(function(item){return(item%2===0)});
-	var array = array.map(function(item){return(item*2)});
-	var array = array.map(function(item){if(item%4===0){return item}});
-	var array = array.reduce(function(prev, cur){return prev + cur});
+	array = array.filter(function(item){return(item%2===0)});
+	array = array.map(function(item){return(item*2)});
+	array = array.map(function(item){if(item%4===0){return item}});
+	array = array.reduce(function(prev, cur){return prev + cur});
 }
-
